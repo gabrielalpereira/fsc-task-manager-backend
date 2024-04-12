38d1c3d10d76a5006e05955bc841ef28e95b3fce
Gabriela Pereira <gabrielalara3010@gmail.com>
2024-04-12 15:52:52 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //Biblioteca
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 //Arquivo
 const connectToDataBase = require("./src/database/mongoose.database");
+const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 connectToDataBase();
 
@@ -14,11 +18,25 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
 });
 
-//JSON
-app.get("/tasks", (req, res) => {
-    const tasks = [{ description: "Estudar programação", isCompleted: false }];
-    res.status(200).send(tasks);
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
-// Servidor local
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.listen(8000, () => console.log("Listening on port 8000!"));
